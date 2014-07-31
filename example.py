70dee4fe0388d9ea568993ec4bd4499e1341bb1e
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-07-31 13:08:40 +0100

    Move build_request into client

--- example.py
+++ example.py
@@ -1,23 +1,11 @@
 from client import StockzClient
 
-def build_request(action, sender, data = None):
-	"""Build a request from an action, sender and data"""
-
-	# Create request array with required fields
-	request = [action, sender]
-
-	# Add data if provided
-	if data is not None:
-		request.append(data)
-
-	return (' ').join(request)
+request = ('hello', 'Jack')
 
 client = StockzClient()
 
-request = build_request("hello", "Jack")
+print(('Request: {0}').format((' ').join(map(str, request))))
 
-print(('Request: {0}').format(request))
-
-response = client.execute(request)
+response = client.send_request(*request)
 
 print(('Response: {0}').format(response))
